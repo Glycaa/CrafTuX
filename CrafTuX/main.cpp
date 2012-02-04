5288c313885214b89d7e43f7a24bcfacd42b2979
Glyca <glycaa@gmail.com>
2012-02-04 11:52:52 +0100

    Added a custom message handler, prefixing all messages.

--- CrafTuX/main.cpp
+++ CrafTuX/main.cpp
@@ -1,11 +1,34 @@
-﻿#include <QtGui/QApplication>
+﻿#include <cstdio>
+#include <cstdlib>
+#include <QtGui/QApplication>
+
 #include "CraftuxHome.h"
+
+void craftuxMessageOutput(QtMsgType type, const char* msg)
+ {
+	 switch (type) {
+	 case QtDebugMsg:
+		 fprintf(stderr, QObject::tr("[Debug] %s\n").toAscii(), msg);
+		 break;
+	 case QtWarningMsg:
+		 fprintf(stderr, QObject::tr("[Warning] %s\n").toAscii(), msg);
+		 break;
+	 case QtCriticalMsg:
+		 fprintf(stderr, QObject::tr("[Critical] %s\n").toAscii(), msg);
+		 break;
+	 case QtFatalMsg:
+		 fprintf(stderr, QObject::tr("[Fatal] %s\n").toAscii(), msg);
+		 abort();
+	 }
+ }
 
 int main(int argc, char *argv[])
 {
-    // On prend nos précautions
+	// On prend nos précautions pour les traductions
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
     QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+
+	qInstallMsgHandler(craftuxMessageOutput);
 
     QApplication a(argc, argv);
     CraftuxHome w;
