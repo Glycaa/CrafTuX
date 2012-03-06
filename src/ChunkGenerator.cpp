d7b7de48d9a81eb451e0dff5746e49bbd77ce4f7
Glyca <glycaa@gmail.com>
2012-03-06 18:10:39 +0100

    The generator produces some... mountains. Still very very experimental!

--- src/ChunkGenerator.cpp
+++ src/ChunkGenerator.cpp
@@ -4,7 +4,7 @@ double Noise(int x, int y)
 {
 	int n = x + y * 57;
 	n = pow((n<<13), n);
-	return ( 1.0 - ( (n * (n * n * 15731 + 789221) + 1376312589) & 0xffffffff) / 1073741824.0);
+	return ( 1.0 - ( (n * (n * n * 15731 + 789221) + 1376312589) /*& 0x7fffffff*/) / 1073741824.0);
 }
 
 double SmoothNoise_1(double x, double y)
