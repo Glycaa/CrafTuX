7670cbbde096a115d60a75577a3972901500ab42
Glyca <glycaa@gmail.com>
2012-04-08 22:15:17 +0200

    You can't place a block where there is already one placed.

--- src/server/events/BlockUseEvent.cpp
+++ src/server/events/BlockUseEvent.cpp
@@ -8,8 +8,8 @@ BlockUseEvent::BlockUseEvent(const BlockPosition& position, Player* player) : Pl
 void BlockUseEvent::perform(Server& server) const
 {
 	int blockId = m_player->inventorySlot(m_player->selectedSlot()).id();
-	// If the player has a block of this id in stock
-	if(m_player->takeOne(blockId)) {
+	// If there is not already a block here the player has a block of this id in stock
+	if(server.world().block(m_blockPosition)->isVoid() && m_player->takeOne(blockId)) {
 		server.world().block(m_blockPosition)->setId(blockId);
 		server.world().chunk(m_blockPosition)->makeDirty();
 	}
