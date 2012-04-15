4227224ca8dc68135c9578eb2d3865ad679ac745
Glyca <glycaa@gmail.com>
2012-04-15 12:08:47 +0200

    Updated french translation.

--- lang/craftux_fr.ts
+++ lang/craftux_fr.ts
@@ -5,23 +5,23 @@
 <context>
     <name>ConnectDialog</name>
     <message>
-        <location filename="../src/gui/ConnectDialog.ui" line="18"/>
-        <location filename="../src/gui/ConnectDialog.ui" line="26"/>
+        <location filename="../src/gui/ui/ConnectDialog.ui" line="18"/>
+        <location filename="../src/gui/ui/ConnectDialog.ui" line="26"/>
         <source>Connect to a server</source>
         <translation>Se connecter à un serveur</translation>
     </message>
     <message>
-        <location filename="../src/gui/ConnectDialog.ui" line="35"/>
+        <location filename="../src/gui/ui/ConnectDialog.ui" line="35"/>
         <source>Address</source>
         <translation>Adresse</translation>
     </message>
     <message>
-        <location filename="../src/gui/ConnectDialog.ui" line="45"/>
+        <location filename="../src/gui/ui/ConnectDialog.ui" line="45"/>
         <source>Port</source>
         <translation>Port</translation>
     </message>
     <message>
-        <location filename="../src/gui/ConnectDialog.ui" line="71"/>
+        <location filename="../src/gui/ui/ConnectDialog.ui" line="71"/>
         <source>Last servers</source>
         <translation>Derniers serveurs</translation>
     </message>
@@ -29,37 +29,37 @@
 <context>
     <name>CraftuxHome</name>
     <message>
-        <location filename="../src/gui/CraftuxHome.ui" line="32"/>
+        <location filename="../src/gui/ui/CraftuxHome.ui" line="32"/>
         <source>Welcome to CrafTuX</source>
         <translation>Bienvenue à CrafTuX</translation>
     </message>
     <message>
-        <location filename="../src/gui/CraftuxHome.ui" line="82"/>
+        <location filename="../src/gui/ui/CraftuxHome.ui" line="82"/>
         <source>Hey, welcome to CrafTuX!</source>
         <translation>Hey, bienvenue à CrafTuX !</translation>
     </message>
     <message>
-        <location filename="../src/gui/CraftuxHome.ui" line="92"/>
+        <location filename="../src/gui/ui/CraftuxHome.ui" line="92"/>
         <source>Play &amp;solo</source>
         <translation>Partie &amp;solo</translation>
     </message>
     <message>
-        <location filename="../src/gui/CraftuxHome.ui" line="99"/>
+        <location filename="../src/gui/ui/CraftuxHome.ui" line="99"/>
         <source>Join a &amp;multiplayer game</source>
         <translation>Rejoindre une partie &amp;multijoueur</translation>
     </message>
     <message>
-        <location filename="../src/gui/CraftuxHome.ui" line="138"/>
+        <location filename="../src/gui/ui/CraftuxHome.ui" line="138"/>
         <source>&amp;Host a game</source>
         <translation>&amp;Héberger une partie</translation>
     </message>
     <message>
-        <location filename="../src/gui/CraftuxHome.ui" line="216"/>
+        <location filename="../src/gui/ui/CraftuxHome.ui" line="216"/>
         <source>&amp;Options</source>
         <translation>&amp;Options</translation>
     </message>
     <message>
-        <location filename="../src/gui/CraftuxHome.ui" line="236"/>
+        <location filename="../src/gui/ui/CraftuxHome.ui" line="236"/>
         <source>&amp;Quit</source>
         <translation>&amp;Quitter</translation>
     </message>
@@ -67,17 +67,17 @@
 <context>
     <name>GameWindow</name>
     <message>
-        <location filename="../src/gui/GameWindow.cpp" line="29"/>
+        <location filename="../src/gui/GameWindow.cpp" line="35"/>
         <source>Initialized OpenGL, version %d.%d</source>
         <translation>OpenGL initialisé en version %d.%d</translation>
     </message>
     <message>
-        <location filename="../src/gui/GameWindow.cpp" line="80"/>
+        <location filename="../src/gui/GameWindow.cpp" line="95"/>
         <source>FPS</source>
         <translation> IPS</translation>
     </message>
     <message>
-        <location filename="../src/gui/GameWindow.cpp" line="82"/>
+        <location filename="../src/gui/GameWindow.cpp" line="97"/>
         <source>Position : </source>
         <translation>Position : </translation>
     </message>
@@ -86,7 +86,7 @@
         <translation type="obsolete"> IPS</translation>
     </message>
     <message>
-        <location filename="../src/gui/GameWindow.cpp" line="103"/>
+        <location filename="../src/gui/GameWindow.cpp" line="118"/>
         <source>The game is paused
 
 You can resume by pressing ESCAPE or quit with C.</source>
@@ -98,133 +98,133 @@ Vous pouvez reprendre la partie en appuyant sur ECHAP ou quitter avec C.</transl
 <context>
     <name>OptionsDialog</name>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="18"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="18"/>
         <source>Options for Craftux client</source>
         <translation>Options pour le client Craftux</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="37"/>
-        <location filename="../src/gui/OptionsDialog.ui" line="54"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="37"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="54"/>
         <source>Game</source>
         <translation>Jeu</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="62"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="62"/>
         <source>Seed</source>
         <translation>Graine</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="107"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="107"/>
         <source>Graphics</source>
         <translation>Graphismes</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="122"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="122"/>
         <source>Performance</source>
         <translation>Performance</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="136"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="136"/>
         <source>Target FPS</source>
         <translation>Images par seconde (FPS)</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="169"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="169"/>
         <source>View distance</source>
         <translation>Distance de vue</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="213"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="213"/>
         <source>Antialiasing</source>
         <translation>Anticrénélage</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="245"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="245"/>
         <source>Texture</source>
         <translation>Texture</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="259"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="259"/>
         <source>Texture filtering</source>
         <translation>Filtrage des textures</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="267"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="267"/>
         <source>None</source>
         <translation>Aucun</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="272"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="272"/>
         <source>Bilinear</source>
         <translation>Bilinéaire</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="277"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="277"/>
         <source>Bilinear with mipmaps</source>
         <translation>Bilinéaire avec mipmaps</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="282"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="282"/>
         <source>Trilinear with mipmaps</source>
         <translation>Trilinéaire avec mipmaps</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="310"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="310"/>
         <source>Key mapping</source>
         <translation>Mappage des touches</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="325"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="325"/>
         <source>Moving</source>
         <translation>Déplacement</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="345"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="345"/>
         <source>Forward</source>
         <translation>Avant</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="359"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="359"/>
         <source>Backwards</source>
         <translation>Arrière</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="415"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="415"/>
         <source>Global</source>
         <translation>Global</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="444"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="444"/>
         <source>Music</source>
         <translation>Musique</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="473"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="473"/>
         <source>Effects</source>
         <translation>Effets</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="352"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="352"/>
         <source>Left</source>
         <translation>Gauche</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="366"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="366"/>
         <source>Right</source>
         <translation>Droite</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="376"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="376"/>
         <source>Jump</source>
         <translation>Sauter</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="390"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="390"/>
         <source>Sound</source>
         <translation>Son</translation>
     </message>
     <message>
-        <location filename="../src/gui/OptionsDialog.ui" line="189"/>
+        <location filename="../src/gui/ui/OptionsDialog.ui" line="189"/>
         <source>Smooth shades</source>
         <translation>Formes douces</translation>
     </message>
@@ -272,89 +272,112 @@ Vous pouvez reprendre la partie en appuyant sur ECHAP ou quitter avec C.</transl
         <translation>Erreur lors de l&apos;ouverture du fichier de configuration %1</translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="15"/>
+        <location filename="../src/main.cpp" line="16"/>
         <source>[Debug] %s
 </source>
         <translation>[Debug] %s
 </translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="18"/>
+        <location filename="../src/main.cpp" line="19"/>
         <source>[Warning] %s
 </source>
         <translation>[Avertissement] %s
 </translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="21"/>
+        <location filename="../src/main.cpp" line="22"/>
         <source>[Critical] %s
 </source>
         <translation>[Critique] %s
 </translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="24"/>
+        <location filename="../src/main.cpp" line="25"/>
         <source>[Fatal] %s
 </source>
         <translation>[Fatal] %s
 </translation>
     </message>
     <message>
-        <location filename="../src/gui/TextureManager.cpp" line="26"/>
-        <location filename="../src/gui/TextureManager.cpp" line="42"/>
         <source>Texture [%1] load failed!</source>
-        <translation>Le chargement de la texture [%1] a échoué !</translation>
+        <translation type="obsolete">Le chargement de la texture [%1] a échoué !</translation>
     </message>
     <message>
-        <location filename="../src/blocks/Blocks.cpp" line="20"/>
+        <location filename="../src/blocks/Blocks.cpp" line="27"/>
         <source>Blocks catalog created, having %1 blocks.</source>
         <translation>Catalogue des blocs créé, il contient %1 blocs.</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/TextureManager.cpp" line="26"/>
+        <source>Texture loading [%1] for &quot;%2&quot; failed!</source>
+        <translation>Le chargement de la texture [%1] du bloc &quot;%2&quot; a échoué !</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/TextureManager.cpp" line="27"/>
+        <source>Successfully loaded &quot;%1&quot; texture [%2].</source>
+        <translation>Chargé la texture [%2] du bloc &quot;%1&quot; !</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/TextureManager.cpp" line="125"/>
+        <source>Textures loaded!</source>
+        <translation>Textures chargées !</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/TextureManager.cpp" line="143"/>
+        <source>Item image loading [%1] for &quot;%2&quot; failed!</source>
+        <translation>Le chargement de l&apos;image [%1] du bloc &quot;%2&quot; a échoué !</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/TextureManager.cpp" line="144"/>
+        <source>Successfully loaded &quot;%1&quot; item image [%2].</source>
+        <translation>Chargé l&apos;image [%2] du bloc &quot;%1&quot; !</translation>
     </message>
 </context>
 <context>
     <name>ServerWidget</name>
     <message>
-        <location filename="../src/gui/ServerWidget.ui" line="12"/>
+        <location filename="../src/gui/ui/ServerWidget.ui" line="12"/>
         <source>Craftux server</source>
         <translation>Serveur Craftux</translation>
     </message>
     <message>
-        <location filename="../src/gui/ServerWidget.ui" line="16"/>
+        <location filename="../src/gui/ui/ServerWidget.ui" line="16"/>
         <source>Overview</source>
         <translation>Aperçu</translation>
     </message>
     <message>
-        <location filename="../src/gui/ServerWidget.ui" line="34"/>
+        <location filename="../src/gui/ui/ServerWidget.ui" line="34"/>
         <source>&amp;Start</source>
         <translation>&amp;Démarrer</translation>
     </message>
     <message>
-        <location filename="../src/gui/ServerWidget.ui" line="41"/>
+        <location filename="../src/gui/ui/ServerWidget.ui" line="41"/>
         <source>Sto&amp;p</source>
         <translation>Arrê&amp;ter</translation>
     </message>
     <message>
-        <location filename="../src/gui/ServerWidget.ui" line="51"/>
+        <location filename="../src/gui/ui/ServerWidget.ui" line="51"/>
         <source>Console</source>
         <translation>Console</translation>
     </message>
     <message>
-        <location filename="../src/gui/ServerWidget.cpp" line="21"/>
+        <location filename="../src/gui/ui/ServerWidget.cpp" line="21"/>
         <source>The server starts</source>
         <translation>Le serveur démarre</translation>
     </message>
     <message>
-        <location filename="../src/gui/ServerWidget.cpp" line="29"/>
+        <location filename="../src/gui/ui/ServerWidget.cpp" line="29"/>
         <source>The server is being stopped</source>
         <translation>Le serveur s&apos;arrête</translation>
     </message>
     <message>
-        <location filename="../src/gui/ServerWidget.cpp" line="37"/>
+        <location filename="../src/gui/ui/ServerWidget.cpp" line="37"/>
         <source>The server is running</source>
         <translation>Le serveur est allumé</translation>
     </message>
     <message>
-        <location filename="../src/gui/ServerWidget.cpp" line="44"/>
+        <location filename="../src/gui/ui/ServerWidget.cpp" line="44"/>
         <source>The server is not running</source>
         <translation>Le serveur est éteint</translation>
     </message>
